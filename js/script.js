8a93f4140002d9acddeb5ccb74a6fdfa4e31ccff
david chapman <chap6680@gmail.com>
2018-05-07 07:47:29 -0600

    final working on all - before cleanup

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 // LEFT OFF - 
 let getAllStudents = document.getElementsByClassName("student-item");
-console.log("total:");
-console.log(getAllStudents.length);
 const getAllStudentsNum = getAllStudents.length;
-const getPaginationNum = Math.floor(getAllStudentsNum/10)+1;
+let getPaginationNum = Math.ceil(getAllStudentsNum / 10);
 
+/* console.log("total:");
+console.log(getAllStudents.length);
+ */
 
 /*this is the main function - 
 - hides all students
@@ -12,17 +13,28 @@ const getPaginationNum = Math.floor(getAllStudentsNum/10)+1;
 /* buttonElement.addEventListener('click', function (event) {
 	alert('Element clicked through function!');
 });
- */  
-function updateStudent10(getBlock) {
+ */
+function updateStudent10(getBlock, currentList) {
+	let currentListNum = currentList.length;
+	getPaginationNum = Math.ceil(currentListNum / 10);
+	/* console.log('firspag');
+	console.log(currentList / 10); */
+
 	//Hide all students
-	[].forEach.call(document.querySelectorAll('.student-item'), function (el) {
+	/* 	[].forEach.call(document.querySelectorAll('.student-item'), function (el) {
+			el.style.display = 'none';
+		}); */
+	console.log('current list:');
+	console.log(currentList);
+	[].forEach.call(getAllStudents, function (el) {
 		el.style.display = 'none';
 	});
 
 	//display the 10 - 
-	for (let i = getBlock-10; i <= Math.min(getBlock -1,getAllStudentsNum-1); i++) {
-		/* console.log(i); */
-		document.getElementsByClassName('student-item')[i].style.display = 'block';
+	for (let i = getBlock - 10; i <= Math.min(getBlock - 1, currentListNum - 1); i++) {
+
+		//document.getElementsByClassName('student-item')[i].style.display = 'block';
+		currentList[i].style.display = 'block';
 	};
 
 	//pagination buttons
@@ -33,80 +45,78 @@ function updateStudent10(getBlock) {
 	});
 
 	/* this gets the block of 10 that was just clicked and then formats that with className of active
-	*/
-	let getPaginationNav = ((getBlock / 10) - 1);
-	
-	document.querySelectorAll(".pagination li a")[getPaginationNav].className = 'active'
-};	
+	 */
+	let getPaginationNav = ((getBlock / 10));
+
+	paginationFormat(getPaginationNav, getPaginationNum);
+	//document.querySelectorAll(".pagination li a")[getPaginationNav].className = 'active'
+};
 
 
-/* function createPagination() OLD WORKING { */
-	let createPaginationString = '<ul>';
-	for (let i = 1; i <= getPaginationNum; i++) {  
+function paginationFormat(currentPagination, totalPagination) {
+/* 	console.log('pag');
+	console.log(currentPagination);
+	console.log(totalPagination);
+ */
+	let createPaginationString = '';
+	document.getElementsByClassName('pagination')[0].innerHTML = createPaginationString;
+
+	createPaginationString = '<ul>';
+
+	for (let i = 1; i <= totalPagination; i++) {
 		createPaginationString = createPaginationString + '<li>';
-		if (i === 1) {
+		if (i === currentPagination) {
 			createPaginationString += '<a class="active" value=1 href="#">'
 		} else {
-			createPaginationString += '<a class="" href="#" value='+i+'>'			
+			createPaginationString += '<a class="" href="#" value=' + i + '>'
 		};
 		createPaginationString += i + '</a></li>'
-	};	
-createPaginationString += ' </ul> </div>';
+	};
 
-document.getElementsByClassName('pagination')[0].innerHTML = createPaginationString;
+	createPaginationString += ' </ul> </div>';
+
+	if (totalPagination === 0) { 
+		createPaginationString = "No Results Found";
+	}
+
+	document.getElementsByClassName('pagination')[0].innerHTML = createPaginationString;
+
+	let tempP = document.querySelectorAll('.pagination ul li');
+	
+	for (let q = 0; q < tempP.length; q++) {
+		tempP[q].addEventListener('click', cluelesseventlistener, false);
+	};
+}
+
+/* function createPagination() OLD WORKING { */
 
 // end of pagination buttons
 
-let createSearchString = '<input type="text" id="studentSearch" name="studentSearch" placeholder="Search for students..."> <button onclick="studentSearchII()">Search</button> <button onclick="clearstudentSearchII()">Clear</button>'
+let createSearchString = '<input type="text" id="studentSearch" name="studentSearch" placeholder="Search for students..."> <button onclick="studentSearchII(10)">Search</button> <button onclick="clearstudentSearchII()">Clear</button>'
 document.getElementsByClassName('student-search')[0].innerHTML = createSearchString;
 
 
 
-updateStudent10(30);
+updateStudent10(30, getAllStudents);
 
 
-function studentSearch() { 
-	let temp = [];
-	let getstudentSearch = document.getElementById("studentSearch").value;
-	console.log(getstudentSearch);
-	let getStudentDetails = document.querySelectorAll('.student-details h3');
-	console.log(getStudentDetails);
-	[].forEach.call(document.querySelectorAll('.student-item h3'), function (el) {
-		temp.push (el.firstChild.data);	
-	});
-	console.log(temp);
-	let picked = temp.indexOf('chen');
-	console.log(picked);
-}
-function cluelesseventlistener(evt) { 
+function cluelesseventlistener(evt) {
 	let tempR = evt.target.innerText * 10;
-/* 	alert(evt.target.innerText);
- */	console.log('innertext');
-	console.log(tempR);
-	updateStudent10(tempR);
+	getstudentSearch = document.getElementById("studentSearch").value;
+	if ((getstudentSearch == '' || getstudentSearch === undefined || getstudentSearch == null || getstudentSearch.length <= 0)) {
+		currentList = document.getElementsByClassName("student-item");
+		updateStudent10(tempR, currentList);
+	} else { 
+		console.log('IXXXMMMMMM HERE');
+		studentSearchII(tempR);
+	};
+	/* 	alert(evt.target.innerText);
+	 
+	console.log('innertext');
+	console.log(tempR);*/
 };
 
-let tempP = document.querySelectorAll('.pagination ul li');
-console.log(tempP);
 
-/* tempP[0].addEventListener('click', cluelesseventlistener);
-/* 	tempP.addEventListener('click', e => {
-				cluelesseventlistener; */
-
-/* function showrunner() { 
- */
-	for (let q = 0; q < tempP.length; q++) {
-		
-		console.log(q);
-	/* 	tempP[q].addEventListener('click', cluelesseventlistener); */
-		tempP[q].addEventListener('click', cluelesseventlistener, false);
-	}; 
-/* };
-
-showrunner; 
- */
-console.log('ok');
-console.log(tempP[1].value);
 
 /* tempP.addEventListener('click', e => {
 	if (e.target === link) {
@@ -116,6 +126,7 @@ console.log(tempP[1].value);
 	}
 }); */
 
+/* playground to get email addresses 
 let getStudentEMailArray = [];
 let getstudentSearch = document.getElementById("studentSearch").value;
 console.log(getstudentSearch);
@@ -125,47 +136,59 @@ console.log(getStudentDetails);
 	getStudentEMailArray.push (el.firstChild.data);	
 });
 console.log(getStudentEMailArray);
+*/
 
-function clearstudentSearchII() { 
+function clearstudentSearchII() {
 	document.getElementById("studentSearch").value = "";
-	updateStudent10(10);
+	updateStudent10(10, getAllStudents);
 };
 
-function studentSearchII() { 
+function studentSearchII(blockNum) {
 	// get the input name to search 
 	let getstudentSearch = document.getElementById("studentSearch").value;
+	let getstudentFilteredList = [];
 	console.log('inputblank');
 	console.log(getstudentSearch);
-	if ((getstudentSearch == '' || getstudentSearch === undefined || getstudentSearch == null || getstudentSearch.length <= 0))  { 
+	if ((getstudentSearch == '' || getstudentSearch === undefined || getstudentSearch == null || getstudentSearch.length <= 0)) {
 		console.log('im in');
-		return updateStudent10(10);
+		return updateStudent10(10, getAllStudents);
 	};
 
-	/*loop through each queryselector and see if the string exists in either the name or email.
-	 */
+	/*loop through each queryselector and see if the string exists in either the name or email.*/
 	[].forEach.call(document.querySelectorAll('.student-item'), function (el) {
 		let tempName = el.querySelector('.student-details h3').innerHTML;
 		let tempEmail = el.querySelector('.student-details .email').innerHTML;
 		if (tempName.includes(getstudentSearch) || tempEmail.includes(getstudentSearch)) {
-			el.style.display = 'block';
+			console.log('loopfilter:');
+			console.log(el);
+			getstudentFilteredList.push(el);
+			/* 			el.style.display = 'block';
+			 */
 		} else {
-			el.style.display = 'none';
-		}	
-	});
 
-	[].forEach.call(document.querySelectorAll('.pagination li a'), function (el) {
-		
-		el.style.display = 'none';
+			/* 			el.style.display = 'none';
+			 */
+		}
 	});
+/* 	if (getstudentFilteredList.length == 0) {
+		console.log('filter');
+		console.log(getstudentFilteredList);
+	}
 
-}
+ */
+	console.log('filtered list:');
+	console.log(blockNum);
+	updateStudent10(blockNum, getstudentFilteredList);
+	
+};
 
 let tempX = (document.querySelectorAll('.student-item'));
-	console.log(tempX);
+console.log(tempX);
 let tempXX = tempX[1].querySelector('.student-details .email').innerHTML;
 console.log(tempXX);
- 
 
+
+console.log(getAllStudents[0]);
 
 
 /*
@@ -187,7 +210,7 @@ console.log(tempXX);
 		}
 		getPaginationNav = document.querySelector(".pagination li").nextSibling.firstChild;
 
-	} */ 
+	} */
 
 /* let y = document.getElementsByClassName(".pagination");
 console.log(y);
@@ -211,7 +234,7 @@ x.className = "active";
 });
 document.getElementsByClassName('student-item')[0].style.display = 'block';
 
-*/  
+*/
 
 /* function createPagination() OLD WORKING { 
 	let createPaginationString = '<ul>';
